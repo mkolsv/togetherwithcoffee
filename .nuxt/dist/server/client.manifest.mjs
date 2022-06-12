a973ad00998e1283aa52b652e807727d5e5b8c34
mkolsv <mwkolosov@gmail.com>
2022-06-12 20:20:40 +0200

    upd gitignore

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -3,6 +3,8 @@ export default {
   "all": [
     "@vite/client",
     "entry.mjs",
+    "assets/styles/core.css",
+    "components/home/header/Header.vue?vue&type=style&index=0&scoped=true&lang.scss",
     "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=true&lang.css",
     "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=true&lang.css",
     "node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=true&lang.css"
@@ -10,6 +12,8 @@ export default {
   "initial": [
     "@vite/client",
     "entry.mjs",
+    "assets/styles/core.css",
+    "components/home/header/Header.vue?vue&type=style&index=0&scoped=true&lang.scss",
     "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=true&lang.css",
     "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=true&lang.css",
     "node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=true&lang.css"
