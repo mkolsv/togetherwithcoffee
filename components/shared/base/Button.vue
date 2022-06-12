92778f2d857d04a40f54921adc22b7f750c9945e
mkolsv <mwkolosov@gmail.com>
2022-06-12 20:23:06 +0200

    add base button

---
+++ components/shared/base/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button
+        :class="buttonClasses"
+        @click="$emit('click', $event)"
+        type="button"
+    >
+        <slot />
+    </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    type: {
+        type: String,
+        required: true
+    }
+});
+
+const buttonClasses = computed(() => `twc-button--${props.type}`);
+</script>
