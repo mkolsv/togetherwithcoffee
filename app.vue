4ada7083c195c36c8a580627da431154b3b97ba8
mkolsv <mwkolosov@gmail.com>
2022-06-14 08:43:39 +0200

    add button type

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
 
 <script>
 import index from './pages';
-import Header from './components/home/header/Header';
+import Header from './components/header/Header';
 
 export default {
     name: 'app',
