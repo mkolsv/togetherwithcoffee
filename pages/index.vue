37ad8c9b9c2542bc09a75423993a17f6013e60fa
mkolsv <mwkolosov@gmail.com>
2022-10-11 23:04:56 +0200

    update base button

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,12 @@
     <main>
         <Slider />
         <FeaturedShops />
+        <About />
     </main>
 </template>
 
 <script setup>
 import Slider from '../components/home/slider/Slider';
 import FeaturedShops from '../components/home/featuredShops/FeaturedShops';
+import About from '../components/home/about/About';
 </script>
